9fbf56e5d98b3e69bd9d260aa5b25bc7969acaa2
Chris <chris_g_50@hotmail.com>
2021-10-15 09:07:20 -0400

    add sass to project + reformatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,30 @@
 import React from "react";
-import "../assets/Header.css";
+import "../assets/Header.scss";
 
 const Header = () => {
-    return <div className="header">
-        <div className="left-top-section">
-            <ul className="left-top-nav">
-                <li>About Ken</li>
-                <li>Contact</li>
-                <li>Track Order</li>
-            </ul>
-        </div>
-        <div className="center-top-section">
-            SPORTS CARDS AND MEMORABILIA
-        </div>
-        <div className="right-top-section"></div>
+    return (
+        <div className="header">
+            <div className="left-top-section">
+                <ul className="left-top-nav">
+                    <li>About Ken</li>
+                    <li>Contact</li>
+                    <li>Track Order</li>
+                </ul>
+            </div>
+            <div className="center-top-section">
+                SPORTS CARDS AND MEMORABILIA
+            </div>
+            <div className="right-top-section"></div>
 
-        <div className="left-center-section"></div>
-        <div className="center-center-section"></div>
-        <div className="right-center-section"></div>
+            <div className="left-center-section"></div>
+            <div className="center-center-section"></div>
+            <div className="right-center-section"></div>
 
-        <div className="left-bottom-section"></div>
-        <div className="center-bottom-section"></div>
-        <div className="right-bottom-section"></div>
-    </div>;
+            <div className="left-bottom-section"></div>
+            <div className="center-bottom-section"></div>
+            <div className="right-bottom-section"></div>
+        </div>
+    );
 };
 
 export default Header;
